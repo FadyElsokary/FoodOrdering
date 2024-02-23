2124cf26dc6f093f45d3c118ac092e64d63984fa
Fady Elsokary <fadymounir21@gmail.com>
2024-02-23 17:33:09 +0200

    Type sage components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import {  View } from '../../components/Themed';
-import products from '@/assets/data/products';
-import ProducrListItem from '@/src/components/ProductListItem';
+import products from '@assets/data/products';
+import ProducrListItem from '@components/ProductListItem';
 
 export default function MenuScreen() {
   return (
